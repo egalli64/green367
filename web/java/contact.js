02bd77e506d5b99e50590f7c4d2ad7a8f5954f4d
Emilia Faraci <emiliafaraci25@gmail.com>
2020-09-05 15:39:54 +0200

    commit changes

--- web/java/contact.js
+++ web/java/contact.js
@@ -1,3 +1,13 @@
-function cancelContent(comment) {
-    comment.value = '';
-  }
+// function validationMail() {
+//   var email = document.module.email.value;
+//   var message = document.module.msh.value;
+//   if ((email.indexOf("@") == (-1)) || (email == "") || (email == "undefined")) {
+//   alert("Inserire un indirizzo email valido.");
+//   document.module.email.focus();
+//   }
+//    else if ((message == "") || (message == "undefined")) {
+//   alert("Inserire un messaggio.");
+//   document.module.msg.focus();
+//   }
+
+//   }
