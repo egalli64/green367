fb912ae92348fcc0b7da455a7e82812306f6f400
Davide Lapi <davide_1502@live.it>
2020-09-10 12:01:33 +0200

    bean

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,4 +1,45 @@
 
 public class User {
+	 private int user_id;
+	 private String user_name;
+	 private String user_pw;
+
+
+  public User() {
+  }
+  public User(int user_id, String user_name,String user_pw) {
+      this.user_id = user_id;
+      this.user_name = user_name;
+      this.user_pw = user_pw;
+  }
+
+  public long getUser_id() {
+      return user_id;
+  }
+
+  public void setUser_id(int user_id) {
+      this.user_id = user_id;
+  }
+
+  public String getUser_name() {
+      return user_name;
+  }
+
+  public void setUser_name(String user_name) {
+      this.user_name = user_name;
+  }
+  
+  public String getUser_pw() {
+      return user_pw;
+  }
+
+  public void setUser_pw(String user_pw) {
+      this.user_pw = user_pw;
+  }
+
+  @Override
+  public String toString() {
+      return "Service [user_id=" + user_id + ", user_name=" + user_name + ", user_pw=" + user_pw +"]";
+  }
 
 }
