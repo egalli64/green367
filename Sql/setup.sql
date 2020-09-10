d60dee1cd2ade841a56f6ae24b0317002b8589ae
Mauro Forte <mauroforte93@gmail.com>
2020-09-10 11:37:37 +0200

    setup.sql

---
+++ Sql/setup.sql
@@ -0,0 +1,9 @@
+-- run it as root
+
+drop user if exists green;
+drop schema if exists green;
+
+create user green identified by 'password';
+create schema green;
+grant all privileges on green.* to green;
+grant alter routine on green.* to green;
