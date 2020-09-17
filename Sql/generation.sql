4cf16c8306c8edad655d8bbc05b246c2a821878b
Emilia Faraci <emiliafaraci25@gmail.com>
2020-09-17 11:18:13 +0200

    commit sql generation

--- Sql/generation.sql
+++ Sql/generation.sql
@@ -1,5 +1,10 @@
 use green;
 
+
+drop table if exists bookings;
+drop table if exists times;
+drop table if exists exams;
+drop table if exists hospitals;
 drop table if exists users;
 
 create table users(
@@ -20,4 +25,79 @@ insert into users(user_id,user_name,user_email,user_password,first_name,last_nam
 insert into users(user_id,user_name,user_email,user_password,first_name,last_name,user_cf,cap) values('5','maaurogreen','mauro@green.it','Greenmauro1','Mauro','Forte','MROFRT93Z08O765P','21098');
 insert into users(user_id,user_name,user_email,user_password,first_name,last_name,user_cf,cap) values('6','emiliagreen','emilia@green.it','Greenemilia1','Emilia','Faraci','EMLFRI98D19I785Q','20095');
 
+commit;
+
+
+create table hospitals(
+	hospital_id integer primary key,
+    hospital_name varchar(50),
+    hospital_address varchar(50),
+    hospital_cap integer,
+    hospital_city varchar (20)
+    );
+    
+insert into hospitals(hospital_id,hospital_name,hospital_address,hospital_cap,hospital_city) values('100', 'Ospedale Sacco', 'Piazza Leonardo', 20100, 'Milano');
+insert into hospitals(hospital_id,hospital_name,hospital_address,hospital_cap,hospital_city) values('101', 'Ospedale Niguarda', 'Piazza Piola', 20100, 'Milano');
+insert into hospitals(hospital_id,hospital_name,hospital_address,hospital_cap,hospital_city) values('102', 'Ospedale San Raffaele', 'Cascina Gobba', 20100, 'Milano');
+
+commit;
+
+
+    
+
+create table exams(
+	exam_id integer primary key auto_increment,
+    exam_name varchar(50),
+    exam_department varchar (50),
+    hospital_id integer,
+    
+    constraint exams_hospital_id_fk foreign key(hospital_id) references hospitals(hospital_id)
+    );
+    
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('1','Pulizia dentale','Odontoiatria','100');    
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('2','Estrazione dente','Odontoiatria','100'); 
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('3','Rimozione carie','Odontoiatria','100'); 
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('4','Pulizia dentale','Odontoiatria','101'); 
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('5','Estrazione dente','Odontoiatria','101'); 
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('6','Rimozione carie','Odontoiatria','101'); 
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('7','Controllo nei','Dermatologia','102'); 
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('8','Controllo nei','Dermatologia','101'); 
+insert into exams(exam_id,exam_name,exam_department,hospital_id) values ('9','Controllo nei','Dermatologia','100'); 
+
+commit;
+
+create table times(
+	time_id integer primary key auto_increment,
+    time_real varchar(5)
+);
+
+insert into times(time_id,time_real) values ('1','10:30');
+insert into times(time_id,time_real) values ('2','11:30');
+insert into times(time_id,time_real) values ('3','12:30');
+
+    
+commit;
+
+
+
+
+create table bookings(
+	booking_id integer primary key auto_increment,
+    user_id integer,
+    exam_id integer,
+    hospital_id integer,
+    booking_date DATE,
+    time_id integer,
+    price varchar(10),
+    
+        constraint bookings_hospital_id_fk foreign key(hospital_id) references hospitals(hospital_id),
+        constraint bookings_user_id_fk foreign key(user_id) references users(user_id),
+		constraint bookings_exam_id_fk foreign key(exam_id) references exams(exam_id),
+        constraint bookings_time_id_fk foreign key(time_id) references times(time_id)
+);
+
+insert into bookings(booking_id,user_id,exam_id,hospital_id,booking_date,time_id,price) values ('1', '2','4','101','2020-10-10','1','100€');
+insert into bookings(booking_id,user_id,exam_id,hospital_id,booking_date,time_id,price) values ('2', '4','5','101','2020-10-11','2','100€');
+insert into bookings(booking_id,user_id,exam_id,hospital_id,booking_date,time_id,price) values ('3', '6','7','102','2020-10-12','3','100€');
+        
 commit;
