f0cc599be6730bcd21d19b7074e4dabb5e4ad1b4
Elisa Mani <elisa.mani.em@gmail.com>
2020-09-18 15:43:10 +0200

    lista servizi

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -6,12 +6,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class Booking
@@ -19,7 +21,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/booking")
 public class Booking extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	@Resource(name = "jdbc/green")
+	private DataSource ds;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -33,15 +36,15 @@ public class Booking extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	           Map<String, String> mappa= new HashMap<String,String>();
-	           mappa.put("1", "OSPEDALE DI VIZZOLO PREDABISSI");
-	           mappa.put("2","OSPEDALE BOLOGNINI - SERIATE");
-	           mappa.put("3","OSPEDALE SERBELLONI - GORGONZOLA");
-	           mappa.put("4","OSPEDALE CROTTA OLTROCCHI-VAPRIO D'ADDA");
-	           mappa.put("5","OSP. GENERALE PROVINCIALE - SARONNO");
-	           mappa.put("6","OSPEDALE PESENTI FENAROLI - ALZANO L.");
-	           mappa.put("7","PRESIDIO OSPEDALIERO DI ROVATO");
-	           mappa.put("8","OSPEDALE S. MARIA DELLE STELLE MELZO");
-	           mappa.put("9","OSPEDALE UBOLDO - CERNUSCO S/NAVIGLIO");
+	           mappa.put("100", "Ospedale di Vizzolo Predissi");
+	           mappa.put("102","OSPEDALE BOLOGNINI - SERIATE");
+	           mappa.put("103","OSPEDALE SERBELLONI - GORGONZOLA");
+	           mappa.put("104","OSPEDALE CROTTA OLTROCCHI-VAPRIO D'ADDA");
+	           mappa.put("105","OSP. GENERALE PROVINCIALE - SARONNO");
+	           mappa.put("106","OSPEDALE PESENTI FENAROLI - ALZANO L.");
+	           mappa.put("107","PRESIDIO OSPEDALIERO DI ROVATO");
+	           mappa.put("108","OSPEDALE S. MARIA DELLE STELLE MELZO");
+	           mappa.put("109","OSPEDALE UBOLDO - CERNUSCO S/NAVIGLIO");
 	           mappa.put("10","OSPEDALE S. PAOLO - MILANO");
 	           for(Integer i=1; i<=mappa.size();i++) {
 	        	   String id= request.getParameter(i.toString());
@@ -50,6 +53,10 @@ public class Booking extends HttpServlet {
 	        		   i=mappa.size();
 	        		   String ospedaleName= mappa.get(id);
 	        		   request.setAttribute("osp", ospedaleName);
+	        		   try(BookingDao bd= new BookingDao(ds)){
+	        			   request.setAttribute("listaServizi",bd.getServizi(Integer.parseInt(id)));
+	        			   
+	        		   }
 	        		   RequestDispatcher rd = request.getRequestDispatcher("booking.jsp");
 	   				rd.forward(request, response);
 	        	   }
